91e39164d4992517130df60ca2c7235f89186523
David Jansson <davidjansson@pop-os.localdomain>
2020-08-05 15:18:27 +0200

    Initial commit

---
+++ accounts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+# from django.db import models
+# from django.contrib.auth.models import AbstractUser
+
+# class User(AbstractUser):
+#    class Meta:
+#        db_table = 'auth_user'
---
+++ painChart/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PainchartConfig(AppConfig):
+    name = 'painChart'
